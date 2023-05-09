60eca15a21f5fc82e5d87d59dcd4b31712ac57eb
vassumani <42170446+vassumani@users.noreply.github.com>
2023-05-09 22:58:13 +1000

    Implement Index Layout - Initial Version
    Added index homepage with interactive map (non-functional) and bar chart. Incorporates code from last commit. Initial layout prototype demonstrated to tutor in Stand Up 2.

--- script/map2.js
+++ script/map2.js
@@ -1,5 +1,37 @@
-function init() {
+function init (){
+    map();
     chart();
+}
+
+function map() {
+    // Set width and height of the SVG canvas
+var w = 850;
+var h = 530;
+
+// Set up the path Configure the projection (center, translate, and scale)
+var projection = d3.geoMercator()
+    .center([0, 20])
+    .translate([w / 2, h / 2])
+    .scale(130);
+
+// Set up the path with the projection
+var path = d3.geoPath()
+    .projection(projection);
+
+// Add the SVG canvas to the body
+var svg = d3.select("#map")
+    .attr("width", w)
+    .attr("height", h);
+
+// Read the GeoJSON file and bind it to a path
+d3.json("csv/map.json").then(function(json) {
+    svg.selectAll("path")
+        .data(json.features)
+        .enter()
+        .append("path")
+        .attr("d", path)
+        .attr("fill", "grey");
+});
 }
 
 function chart() {
@@ -75,4 +107,5 @@ function chart() {
             });
     });
 }
-window.onload = init; // execute function on script load
+
+window.onload = init;
